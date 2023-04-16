a0be44ca4b3eeb79e544986680d40028f1b2315a
Kim Coleman <kim@strangerstudios.com>
2023-04-16 12:08:11 -0400

    Initial block theme setup

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# blockville
-Block based theme for WordPress community sites.
+# [Blockville](https://www.strangerstudios.com) #
+
+### Welcome to the Blockville GitHub Repository
+
+Blockville is a block-based theme for WordPress Community websites. The theme is specifically designed for WordPress membership sites powered by [Paid Memberships Pro](https://www.paidmembershipspro.com).
+
+## Bugs ##
+If you find an issue/bug, let us know by [creating a detailed GitHub issue](https://github.com/strangerstudios/blockville/issues/new/choose).
+
+## Support ##
+This is a developer's portal for Blockville. We do not offer support on this channel. **Any support related questions should be directed to [strangerstudios.com](https://www.strangerstudios.com).**
+
+## Contributing to Blockville ##
+We encourage and welcome any contribution to Blockville. Please read the [guidelines for contributing](https://github.com/strangerstudios/blockville/blob/dev/.github/CONTRIBUTING.md) to this repository.
+
+There are various **ways to help the development** of Blockville:
+
+1. Report [bugs/issues](https://github.com/strangerstudios/blockville/issues/new/choose) on GitHub.
+2. Work on any issues by submitting a Pull Request.
--- functions.php
+++ functions.php
@@ -1,17 +1,17 @@
 <?php
 /**
- * Functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ * Blockville functions and definitions.
  *
  * @package Blockville
- * @since 0.1.0
+ *
+ * @since [version]
+ * @version [version]
  */
 
 /**
  * Enqueue the style.css file.
  * 
- * @since 0.1.0
+ * @since [version]
  */
 function blockville_styles() {
 	wp_enqueue_style(
@@ -26,81 +26,13 @@ add_action( 'wp_enqueue_scripts', 'blockville_styles' );
 /**
  * Enable theme support for block editor.
  * 
- * @since 0.1.0
+ * @since [version]
  */
-
 if ( ! function_exists( 'blockville_setup' ) ) {
 	function blockville_setup() {
-            add_theme_support( 'wp-block-styles' );
-            add_editor_style( 'style.css' );
-            add_editor_style( 'editor.css' );
-        }
+			add_theme_support( 'wp-block-styles' );
+			add_editor_style( 'style.css' );
+			add_editor_style( 'editor.css' );
+		}
 }
 add_action( 'after_setup_theme', 'blockville_setup' );
-
-
-//here, I am making the content inside custom-template.html become a default template. This means that
-//it will have pre-made blocks when I choose it in the admin screen. 
-function prefix_default_page_template( $settings ) {
-	$settings['defaultBlockTemplate'] = file_get_contents( get_theme_file_path( 'templates/custom-template.html' ) );;
-	return $settings;
-}
-add_filter( 'block_editor_settings_all', 'prefix_default_page_template' );
-
-//this function creates a section on the admin section called books in the form of an archive screen
-//Like a post, you can add, edit, and view posts. When you click add new, it has a default block template for this section
-//the object "template" is making that default template.
-function myplugin_register_book_post_type() {
-    $args = array(
-        'public' => true,
-        'label'  => 'Books',
-        'show_in_rest' => true,
-        'template' => array(
-            array( 'core/columns', array(), array(
-                array( 'core/column', array(), array(
-                    array( 'core/image', array() ),
-                ) ),
-                array( 'core/column', array(), array(
-                    array( 'core/paragraph', array(
-                        'placeholder' => 'Add a inner paragraph'
-                    ) ),
-                ) ),
-            ) )
-        ),
-    );
-    register_post_type( 'book', $args );
-}
-add_action( 'init', 'myplugin_register_book_post_type' );
-
-
-// <--------------------------Other way to add default post type ------------------->
-
-// function prefix_filter_book_content( $content, $post ) {
-// 	if ( $post->post_type === 'book' ) {
-// 		$content ='<!-- wp:columns -->
-// 		<div class="wp-block-columns"><!-- wp:column -->
-// 		<div class="wp-block-column"><!-- wp:image -->
-// 		<figure class="wp-block-image"><img alt=""/></figure>
-// 		<!-- /wp:image --></div>
-// 		<!-- /wp:column -->
-
-// 		<!-- wp:column -->
-// 		<div class="wp-block-column"><!-- wp:paragraph -->
-// 		<p></p>
-// 		<!-- /wp:paragraph --></div>
-// 		<!-- /wp:column --></div>
-// 		<!-- /wp:columns -->';
-// 	}
-// 	return $content;
-// }
-// add_filter( 'default_content', 'prefix_filter_book_content', 10, 2 );
-
-
-// function myplugin_register_template() {
-//     $post_type_object = get_post_type_object( 'book' );
-//     $post_type_object->template = array(
-//         array( 'core/image' ),
-//     );
-// }
-// add_action( 'init', 'myplugin_register_template' );
-//Changes layout of block template above to make it single column. Not double
