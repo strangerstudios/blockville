9e174df41a28391c5317076b19ff11cfaade782d
Brian Santos <briancapellan10@gmail.com>
2023-05-21 15:41:09 -0400

    added a single.html file for events post type and made the admin styled the look of the 2 blocks for the admin screen

--- our-blocks/events.php
+++ our-blocks/events.php
@@ -28,13 +28,13 @@
                 <div class="title"><h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3></div>
                 <div class="description">
                     <p>
-                    <?php 
-                    if(has_excerpt()) {
-                        echo get_the_excerpt();
-                    } else {
-                        echo wp_trim_words(get_the_excerpt(), 18);
-                    }
-                    ?>
+                        <?php 
+                        if(has_excerpt()) {
+                            echo get_the_excerpt();
+                        } else {
+                            echo wp_trim_words(get_the_excerpt(), 18);
+                        }
+                        ?>
                     </p>
                 </div>
                 <div class="date">
